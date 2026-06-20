c94a1c4ac11717cbbe05e5d654b4d31199d10d6a
unknown <velmuruganmahalingam>
2026-06-20 15:16:33 +0530

    cors resolver

--- server/server/src/main.ts
+++ server/server/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser())
   app.enableCors({
-    origin:'http://localhost:3000',
+    origin:['http://localhost:3000','http://18.61.227.188:3000'],
     credentials:true,
   })
   app.useGlobalPipes(
